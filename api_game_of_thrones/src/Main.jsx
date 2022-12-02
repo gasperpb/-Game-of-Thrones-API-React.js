761318f17a06689877596cf17ee08b548bd97b46
gasperpb <achillespb@hotmail.com>
2022-12-02 12:37:31 -0300

    pagination

--- api_game_of_thrones/src/Main.jsx
+++ api_game_of_thrones/src/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 import { Character } from './Character'
 import { Pagination } from './Pagination'
 import { useFetchData } from './useFetchData'
 
 export const Main = () => {
     const { data } = useFetchData();
-
+    const [currentePage, setCurrentPage] = useState(1);
+    const [recordsPerPage] = useState(8);
+    const indexOfLastRecord = currentePage * recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+    const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+    const nPages = Math.ceil(data.length / recordsPerPage)
     return (
         <div>
             <div className='character-container'>
@@ -14,7 +20,9 @@ export const Main = () => {
                 ))}
 
             </div>
-            <Pagination />
+            <Pagination
+                nPages={nPages}
+                currentePage={currentePage} />
         </div>
     )
 }
--- api_game_of_thrones/src/Pagination.jsx
+++ api_game_of_thrones/src/Pagination.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-export const Pagination = () => {
-    return (
-        <div>Pagination</div>
-    )
+export const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+    const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+    return <ul className='character-pagination'>
+        {pageNumbers}
+    </ul>
 }
